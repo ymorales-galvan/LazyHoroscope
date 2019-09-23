b19bf7136356b0298a52429b450df80bcc8982b5
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-09-23 16:29:45 -0700

    Asked for first and last name. The end consisted of getting the initials.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Yuliana
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string firstName;
+string lastName; 
   //get user input
+cout<< "What is your first name? \n" ;
+ cin>>firstName; 
+cout<< "What is your last name? \n ";
+cin>>lastName;
 
   //tell fortune
+ 
+cout<< "Welcome, " <<firstName [0]  <<lastName[0]<<  " ,here is your fortune... ";
 
   return 0;
 }
