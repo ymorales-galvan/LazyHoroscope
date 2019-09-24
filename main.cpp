e5b15f617dd40adb9c8abbc913ae8f8021e1b410
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-09-23 17:20:57 -0700

    added the lucky number

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ string lastName;
   //get user input
 cout<< "What is your first name? \n" ;
 cin>>firstName; 
-cout<< "What is your last name? \n ";
+cout<< "What is your last name? \n";
 cin>>lastName;
 
   //tell fortune
  
-cout<< "Welcome, " <<firstName [0]<<"."  <<lastName[0]<<  ".,here is your fortune..." <<endl;
+cout<<"Welcome, " <<firstName [0]<<"."  <<lastName[0]<<  ".,here is your fortune..." <<endl;
 int lucky = firstName.length ();
 cout<< "Your lucky number is " <<lucky <<endl;
   return 0;
