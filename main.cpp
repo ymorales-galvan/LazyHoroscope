6c8574fb9abd84835b8b6b86f5de31c1604311fc
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-09-25 16:48:43 -0700

    part b

--- main.cpp
+++ main.cpp
@@ -20,5 +20,27 @@ cin>>lastName;
 cout<<"Welcome, " <<firstName [0]<<"."  <<lastName[0]<< "., here is your fortune..." <<endl;
 int lucky = firstName.length ();
 cout<< "your lucky number is " <<lucky <<endl;
+
+
+if ( firstName.at (0) == 'A' || firstName.at (0) == 'a' || firstName.at (0) == 'E'|| firstName.at (0) == 'e' || firstName.at (0) == 'I' || firstName.at (0) == 'i' || firstName.at (0) == 'O' || firstName.at (0) == 'o'|| firstName.at (0) == 'U'|| firstName.at (0) == 'u' )
+{
+        cout<< "You are destined to be famous " <<endl ;
+ }
+else
+{
+        cout<< "You should keep a low profile" <<endl ;
+}
+
+int index;
+index = lastName.length()-1;
+
+if ( lastName.at (0) == 'A' || lastName.at (0) == 'a' || lastName.at (0) == 'E' || lastName.at (0) == 'e' ||  lastName.at (0) == 'I' || lastName.at (0) == 'i' || lastName.at (0) == 'O' || lastName.at (0) =='o' || lastName.at (0) == 'U'|| lastName.at (0) == 'u' )
+{
+        cout<< "You have already met your true love" <<endl;
+}
+else
+ {
+         cout<< "Have a nice day" <<endl;
+ } 
   return 0;
 }
